c2df1fde2919ea45800d4b7d737cfc1d5d4a3fc6
Isaac <ries0163@umn.edu>
2022-03-25 21:57:51 -0500

    Commited the updated files that I had previously failed to commit

--- app/src/main/java/com/example/codegreen/MainActivity.java
+++ app/src/main/java/com/example/codegreen/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.codegreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.example.codegreen.ui.userprofile.Milestone;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,70 @@ import com.example.codegreen.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    public static final String EXTRA_USERNAME = "com.example.codegreen.USERNAME";
+    public static final String EXTRA_LOCATION = "com.example.codegreen.LOCATION";
+    public static final String EXTRA_USERABOUT = "com.example.codegreen.USERABOUT";
+
+    private User user;
+
+    public AppBarConfiguration appBarConfiguration;
+
+    private User makeGenericUser() {
+        User user = new User();
+        user.setUsername("Default username.");
+        user.setLocation("Unset");
+        user.setUserAbout("Describe yourself!");
+// Some generic sounding milestones.
+        user.addToMilestones(
+                new Milestone(
+                        "Code Green",
+                        "You have downloaded and started using Code Green!",
+                        R.drawable.ic_home_black_24dp,
+                        0));
+        user.addToMilestones(
+                new Milestone(
+                        "First Scanning",
+                        "You have scanned your first item!",
+                        R.drawable.ic_camera_black_24dp,
+                        1));
+        user.addToMilestones(
+                new Milestone(
+                        "Top Half",
+                        "You made the top half of users in your area!",
+                        R.drawable.ic_notifications_black_24dp,
+                        2));
+
+        user.addToMilestones(
+                new Milestone(
+                        "One Month of Green",
+                        "You have used Code Green for one month!",
+                        R.drawable.ic_dashboard_black_24dp,
+                        3));
+
+        user.addToMilestones(
+                new Milestone(
+                        "Getting to Know You",
+                        "You have set up your User Profile!",
+                        R.drawable.ic_gear_black_24dp,
+                        4));
+        user.setFavoriteMilestone(user.getMilestones().get(2));
+        return user;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        int themeId = R.style.FontSizeNormal;
+        if (user.isLargeText()) {
+            themeId = R.style.FontSizeLarge;
+        }
+        setTheme(themeId);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        user = makeGenericUser();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -34,4 +98,15 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    // Based on Android Studio tutorials, allows use of back button (for things like User Profile Milestones Fragment).
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
